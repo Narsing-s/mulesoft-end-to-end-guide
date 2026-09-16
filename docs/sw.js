90374274d5a2be1c9df1506ded5c245cdb638027
Narsingarao Beesetti <narsingbeesetti006@gmail.com>
2026-09-16 13:11:07 +0530

    Refresh learning portal cache after removing documentation hub

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE='mulejourney-v11';
+const CACHE='mulejourney-v12';
 const CORE=['./','./index.html','./login.html','./account.html','./releases.html','./email-test.html','./styles.css','./app.js','./auth.js','./extra-content.js','./link-fix.js','./email-config.js','./manifest.webmanifest','./LOGIN.md','./EMAIL-SETUP.md'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
